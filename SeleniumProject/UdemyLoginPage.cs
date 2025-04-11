9abd34ea6e4d11d7f6f58f9700efd73c5869e9f9
Saicharan Punnam <Saicharanpunnam0011@hotmail.com>
2025-04-11 07:28:02 +0530

    expectedproducts and actual products both same it will add in cart and checkout.

--- SeleniumProject/UdemyLoginPage.cs
+++ SeleniumProject/UdemyLoginPage.cs
@@ -19,14 +19,28 @@ namespace SeleniumProject
         [Test]
         public void EndToEndFlow()
         {
+            string[] expectedProducts = { "iphone X", "Nokia Edge", "Blackberry" }; // Fixed typo in variable name
             dr.FindElement(By.CssSelector("#username")).SendKeys("rahulshettyacademy");
             dr.FindElement(By.CssSelector("#password")).SendKeys("learning");
             dr.FindElement(By.CssSelector(".text-info span:nth-child(1) input")).Click();
-            dr.FindElement(By.CssSelector(".text-info span:nth-child(1) input")).Click();
             dr.FindElement(By.CssSelector("#signInBtn")).Click();
-            WebDriverWait wait  = new WebDriverWait(dr, TimeSpan.FromSeconds(8));
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("dsskldl;ksl"));
-            dr.Close();
+            WebDriverWait wait = new WebDriverWait(dr, TimeSpan.FromSeconds(10));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.PartialLinkText("Checkout")));
+            IList<IWebElement> products = dr.FindElements(By.TagName("app-card"));
+            foreach (IWebElement product in products)
+            {
+                if (expectedProducts.Contains(product.FindElement(By.CssSelector(".card-title a")).Text)) // Fixed typo in variable name
+                {
+                    //IWebElement button = product.FindElement(By.CssSelector(".card-footer button"));
+                    //((IJavaScriptExecutor)dr).ExecuteScript("arguments[0].click();", button);
+
+                    product.FindElement(By.CssSelector(".card-footer button")).Click();
+                }
+                TestContext.Progress.WriteLine(product.FindElement(By.CssSelector(".card-title a")).Text);
+            }
+            
+            dr.FindElement(By.PartialLinkText("Checkout")).Click();
+            //dr.Close();
         }
     }
 }
